dab511ce0c3a45d145b7376662f63400e8650553
Chao Chen <cchen@nyu.edu>
2021-07-16 16:02:33 -0400

    update stucture

--- generating_queries/generate_training_tuples_cc_baseline.py
+++ generating_queries/generate_training_tuples_cc_baseline.py
@@ -12,27 +12,6 @@ import config as cfg
 import scipy.io as sio
 import torch
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-base_path = cfg.DATASET_FOLDER
-
-runs_folder = "dm_data"
-filename = "gt_pose.mat"
-pointcloud_fols = "/pointcloud_20m_10overlap/"
-
-print("cfg.DATASET_FOLDER:"+str(cfg.DATASET_FOLDER))
-
-cc_dir = "/mnt/ab0fe826-9b3c-455c-bb72-5999d52034e0/deepmapping/"
-all_folders = sorted(os.listdir(os.path.join(cc_dir,runs_folder)))
-
-folders = []
-
-# All runs are used for training (both full and partial)
-index_list = range(len(all_folders))
-print("Number of runs: "+str(len(index_list)))
-for index in index_list:
-    folders.append(all_folders[index])
-print(folders)
-
 
 #####For training and test data split#####
 
@@ -67,62 +46,84 @@ def construct_query_dict(df_centroids, filename):
     print("Done ", filename)
 
 
-# Initialize pandas DataFrame
-df_train = pd.DataFrame(columns=['file','x','y'])
-df_test = pd.DataFrame(columns=['file','x','y'])
+def generate():
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    base_path = cfg.DATASET_FOLDER
 
-df_files_test = []
-df_files_train =[]
+    runs_folder = "dm_data"
+    filename = "gt_pose.mat"
+    pointcloud_fols = "/pointcloud_20m_10overlap/"
 
-df_locations_tr_x = []
-df_locations_tr_y = []
-df_locations_ts_x = []
-df_locations_ts_y = []
+    print("cfg.DATASET_FOLDER:"+str(cfg.DATASET_FOLDER))
 
-for folder in folders:
-    df_locations = sio.loadmat(os.path.join(
-        cc_dir,runs_folder,folder,filename))
+    cc_dir = "/mnt/ab0fe826-9b3c-455c-bb72-5999d52034e0/deepmapping/"
+    all_folders = sorted(os.listdir(os.path.join(cc_dir,runs_folder)))
+    print("all_folders:"+str(all_folders))
+
+    folders = []
+
+    # All runs are used for training (both full and partial)
+    index_list = range(len(all_folders))
+    print("Number of runs: "+str(len(index_list)))
+    for index in index_list:
+        folders.append(all_folders[index])
+    print(folders)
+
+    # Initialize pandas DataFrame
+    df_train = pd.DataFrame(columns=['file','x','y'])
+    df_test = pd.DataFrame(columns=['file','x','y'])
+
+    df_files_test = []
+    df_files_train =[]
+
+    df_locations_tr_x = []
+    df_locations_tr_y = []
+    df_locations_ts_x = []
+    df_locations_ts_y = []
+
+    for folder in folders:
+        df_locations = sio.loadmat(os.path.join(
+                      cc_dir,runs_folder,folder,filename))
+
+        df_locations = df_locations['pose']
+        df_locations = torch.tensor(df_locations, dtype = torch.float).cpu()
+        #2038 Training 10 testing
+        test_index = random.sample(range(len(df_locations)), k=10)
+        train_index = list(range(df_locations.shape[0]))
+        
+        for i in test_index:
+            train_index.remove(i)
+
+        df_locations_tr_x.extend(list(df_locations[train_index,0]))
+        df_locations_tr_y.extend(list(df_locations[train_index,1]))
+        df_locations_ts_x.extend(list(df_locations[test_index,0]))
+        df_locations_ts_y.extend(list(df_locations[test_index,1]))
+
+        all_files = list(sorted(os.listdir(os.path.join(cc_dir,runs_folder,folder))))
+        all_files.remove('gt_pose.mat')
+        all_files.remove('gt_pose.png')
+
+        for (indx, file_) in enumerate(all_files):            
+            if indx in test_index:
+                df_files_test.append(os.path.join(cc_dir,runs_folder,folder,file_))
+            else:
+                df_files_train.append(os.path.join(cc_dir,runs_folder,folder,file_))                     
     
-    df_locations = df_locations['pose']
-    df_locations = torch.tensor(df_locations, dtype = torch.float).cpu()
-
-    #2038 Training 10 testing
-    test_index = random.sample(range(len(df_locations)), k=10)
-    train_index = list(range(df_locations.shape[0]))
-    for i in test_index:
-        train_index.remove(i)
+    print("df_locations_tr_x:"+str(len(df_locations_tr_x)))
+    print("df_files_test:"+str(len(df_files_test)))
     
-    df_locations_tr_x.extend(list(df_locations[train_index,0]))
-    df_locations_tr_y.extend(list(df_locations[train_index,1]))
-    df_locations_ts_x.extend(list(df_locations[test_index,0]))
-    df_locations_ts_y.extend(list(df_locations[test_index,1]))
+    df_train = pd.DataFrame(list(zip(df_files_train, df_locations_tr_x, df_locations_tr_y)),
+                           columns =['file','x', 'y'])
+    df_test = pd.DataFrame(list(zip(df_files_test, df_locations_ts_x, df_locations_ts_y)),
+                           columns =['file','x', 'y'])
+    print("Number of training submaps: "+str(len(df_train['file'])))
+    print("Number of non-disjoint test submaps: "+str(len(df_test['file'])))
 
-    
-    all_files = list(sorted(os.listdir(os.path.join(cc_dir,runs_folder,folder))))
-    all_files.remove('gt_pose.mat')
-    all_files.remove('gt_pose.png')
+    print("df_train:"+str(len(df_train)))
+    print("df_test:"+str(len(df_test)))
 
-    for (indx, file_) in enumerate(all_files):
-        if indx in test_index:
-            df_files_test.append(os.path.join(cc_dir,runs_folder,folder,file_))
-        else:
-            df_files_train.append(os.path.join(cc_dir,runs_folder,folder,file_))
+    construct_query_dict(df_train,"training_queries_baseline.pickle")
+    construct_query_dict(df_test,"test_queries_baseline.pickle")
 
-print("df_locations_tr_x:"+str(len(df_locations_tr_x)))
-print("df_files_test:"+str(len(df_files_test)))
-
-df_train = pd.DataFrame(list(zip(df_files_train, df_locations_tr_x, df_locations_tr_y)),
-                                               columns =['file','x', 'y'])
-df_test = pd.DataFrame(list(zip(df_files_test, df_locations_ts_x, df_locations_ts_y)),
-                                               columns =['file','x', 'y'])
-
-
-
-print("Number of training submaps: "+str(len(df_train['file'])))
-print("Number of non-disjoint test submaps: "+str(len(df_test['file'])))
-
-print("df_train:"+str(len(df_train)))
-print("df_test:"+str(len(df_test)))
-
-construct_query_dict(df_train,"training_queries_baseline.pickle")
-construct_query_dict(df_test,"test_queries_baseline.pickle")
+if __name__ == "__main__":
+    generate()
