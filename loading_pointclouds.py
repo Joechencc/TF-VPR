cf9691338b3e24d2371a25a02b308f626616cc74
chao chen <cc7287@nyu.edu>
2022-01-19 16:53:57 -0500

    update feature+aug

--- loading_pointclouds.py
+++ loading_pointclouds.py
@@ -69,11 +69,11 @@ def load_pc_files(filenames,full_path):
 def load_image_file(filename, full_path=False):
     if full_path:
         image = cv2.imread(filename)
-        dim = (64*4,64)
+        dim = (cfg.SIZED_GRID_X,cfg.SIZED_GRID_Y)
         image = cv2.resize(image, dim,interpolation = cv2.INTER_AREA)
     else:
-        image = cv2.imread(os.path.join("/mnt/NAS/home/yuhang/videomap_v2/Adrian/", filename))
-        dim = (64*4,64)
+        image = cv2.imread(os.path.join("/mnt/NAS/home/yiming/habitat_4/train/", filename))
+        dim = (cfg.SIZED_GRID_X,cfg.SIZED_GRID_Y)
         image = cv2.resize(image, dim,interpolation = cv2.INTER_AREA)
         # print("image2:"+str(image.shape))
         # assert(0)
